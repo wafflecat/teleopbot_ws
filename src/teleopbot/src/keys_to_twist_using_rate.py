0aea85c0829c7b3f496319c27c51b43644bcdb55
Isaac Ng <xyng735@gmail.com>
2019-07-01 16:16:46 +0800

    Fix yaw control

--- src/teleopbot/src/keys_to_twist_using_rate.py
+++ src/teleopbot/src/keys_to_twist_using_rate.py
@@ -5,7 +5,7 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
 key_mapping = { 'w': [ 0, 1], 'x': [0, -1], 
-                'a': [-1, 0], 'd': [1,  0], 
+                'a': [1, 0], 'd': [-1,  0], 
                 's': [ 0, 0] }
 g_last_twist = None 
 
